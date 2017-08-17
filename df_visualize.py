55e23edae0c097a61e7e19516ba8679d68f65ab2
Lisa Ding <palisade0401@gmail.com>
2017-08-17 09:49:39 -0500

    update df_visualize function

--- df_visualize.py
+++ df_visualize.py
@@ -3,37 +3,37 @@ import matplotlib.pyplot as plt
 
 def df_visualize(df):
     """
-    Plot the dataframe with day, peaking values and the predicted value
+    Plot the dataframe with day, peaking values and the predicted value (if there's any)
     Args:
         :param (dataframe): columns in current dataframe are listed below:
-            1> column 0: day; column 10: date
-            2> column 1-5: peak1, peak2, peak3, peak4, peak5
-            3> column 6-9: peak_final, standard_data, peak_final_predicted, peak_prediction
+            - column 0: day; column 10: date
+            - column 1-5: peak1, peak2, peak3, peak4, peak5
+            - column 6-9: peak_final, standard_data, peak_final_predicted, peak_prediction
     """
 
     x = df['day']
-    y_peak1 = df['peak1']
-    y_peak2 = df['peak2']
-    y_peak3 = df['peak3']
-    y_peak4 = df['peak4']
-    y_peak5 = df['peak5']
-    y_final = df['peak_final']
-    y_standard = df['standard_data']
 
-    # plot all peaking values with blue dots
-    plt.scatter(x, y_peak1, color='b')
-    plt.scatter(x, y_peak2, color='b')
-    plt.scatter(x, y_peak3, color='b')
-    plt.scatter(x, y_peak4, color='b')
-    plt.scatter(x, y_peak5, color='b')
+    basic_scatter_plot(x, 'peak1')
+    basic_scatter_plot(x, 'peak2')
+    basic_scatter_plot(x, 'peak3')
+    basic_scatter_plot(x, 'peak4')
+    basic_scatter_plot(x, 'peak5')
+    basic_scatter_plot(x, 'peak_final')
 
-    # plot our final prediction with red dots
-    plt.scatter(x, y_final, color='r')
-
-    # plot standard data with yellow lines
-    plt.plot(x, y_standard, color='y')
+    if df['standard_data'].notnull().values.any():
+        y_standard = df['peak1']
+        plt.plot(x, y_standard, color='y')
 
     plt.show()
+
+
+def basic_scatter_plot(df, x, y):
+    """
+    Plot scatter plot given x and y axis.
+    """
+    if (df[y].notnull().values.any()):
+        y_peak = df[y]
+        plt.scatter(x, y_peak, color='b')
 
 
 # for testing purpose:
